3fc4034152425b4b9f3c79c8b984d52962ad9d19
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-26 09:34:23 +0530

    tailwind css added

--- src/Redux/Counter.jsx
+++ src/Redux/Counter.jsx
@@ -7,11 +7,31 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Counter</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    <div className="text-center mt-11">
+      <h1 className="text-red-500 font-bold text-5xl mb-9">
+        Counter-Application
+      </h1>
+      <div className="flex justify-center">
+        <div className="pt-40 border border-sky-600 bg-sky-200 w-2/4 rounded-lg">
+          <p className="text-9xl mb-32 font-bold text-blue-600">
+            Count:<b className=" text-white font-bold"> {count}</b>
+          </p>
+          <div className="mb-10">
+            <button
+              onClick={() => dispatch(increment())}
+              className="mr-9 text-3xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            >
+              Increment
+            </button>
+            <button
+              onClick={() => dispatch(decrement())}
+              className="text-3xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux"; // Import the Provider
 import store from "./Redux/Store.jsx";
-
+import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
